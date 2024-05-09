b6c071b99605d841e2633fcb9d1131bbc005f480
Shubham Sharma <76767061+Mobiance@users.noreply.github.com>
2024-05-09 22:41:57 +0530

    add base handlers

--- internals/handlers/getBase.go
+++ internals/handlers/getBase.go
@@ -2,9 +2,21 @@ package handlers
 
 import (
 	"github.com/labstack/echo/v4"
-	"github.com/mobiance/goblog/views/shared"
+	"github.com/mobiance/goblog/views/components"
+	"github.com/mobiance/goblog/views/pages"
 )
 
 func GetBase(c echo.Context) error {
-    return render(c, shared.Page( "Hello" ))
+    return render(c, pages.HomePage() )
+}
+
+func GetProjects(c echo.Context) error {
+    return render(c, pages.ProjectsPage() )
+}
+
+func GetHero(c echo.Context) error {
+    return render(c, components.Hero() )
+}
+func GetAbout(c echo.Context) error {
+    return render(c, components.About() )
 }
