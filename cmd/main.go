516f36cbfaad13165d8acc5dca49b577778cb464
Shubham Sharma <76767061+Mobiance@users.noreply.github.com>
2024-05-09 22:41:44 +0530

    add base routes

--- cmd/main.go
+++ cmd/main.go
@@ -10,12 +10,16 @@ func main() {
     // Create a new echo instance
     e := echo.New()
 
+    e.Static("/static", "/static")
     // Add middleware
     e.Use(middleware.Logger())
     e.Use(middleware.Recover())
     
     Page := handlers.GetBase
     e.GET("/", Page)
+    e.GET("/projects", handlers.GetProjects)
+    e.GET("/hero", handlers.GetHero)
+    e.GET("/about", handlers.GetAbout)
     // Start the server
-    e.Logger.Fatal(e.Start(":3000"))
+    e.Logger.Fatal(e.Start(":4000"))
 }
